9f0b738f8c1f8b926bd394ae92b98dc62f2646aa
Deepak Raj <47591132+Aurjay@users.noreply.github.com>
2024-04-07 15:22:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
 
-import os
-from deep_translator import GoogleTranslator
-xs=[]
-#Here you should replace with your path for file.
-path_dir = "C:\\Users\\Deepak\\Desktop\\azcopy_windows_amd64_10.12.2\\tum\\TUMAI_dataset\\pdp_dataset\\KellerSports"
-Keller_sports = os.listdir(path_dir)
-#Here the product description of all different folders are accessed.
-path_list = []
-for y in Keller_sports:
-    path_list.append("{}\\{}\\{}".format(path_dir, y, 'product_description.txt'))
-for z in path_list:
-    with open(z,'r',encoding = 'utf8') as f:
-        flat_list = [word for line in f for word in line.split()]
-#Google translator has a 5000 characters limit.
-    translated = GoogleTranslator(source='de', target='en').translate_batch(flat_list)
-    translatedd = " ".join(translated)
-    h = open(z, 'w', encoding='utf8')
-    h.write(translatedd)
-print("Translated")
+(xs, ys),_ = datasets.mnist.load_data()
+
+xs = tf.convert_to_tensor(xs, dtype=tf.float32) / 255.
+db = tf.data.Dataset.from_tensor_slices((xs,ys))
+db = db.batch(32).repeat(10)
+
+
+model = Sequential([layers.Dense(128, activation='relu'),
+                     layers.Dense(256, activation='relu'),
+                     layers.Dense(128, activation='relu'),
+                     layers.Dense(10)])
+model.build(input_shape=(None, 28*28))
+model.summary()
+
+optimizer = optimizers.SGD(lr=0.01)
+acc_meter = metrics.Accuracy()
+
+for step, (x,y) in enumerate(db):
+
+    with tf.GradientTape() as tape:
+        x = tf.reshape(x, (-1, 28*28))
+        out = model(x)
+        y_onehot = tf.one_hot(y, depth=10)
+        loss = tf.square(out-y_onehot)
+        loss = tf.reduce_sum(loss) / 32
+
+
+    acc_meter.update_state(tf.argmax(out, axis=1), y)
+
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    if step % 400==0:
+        print('Loss:', float(loss), ', Acc:', acc_meter.result().numpy())
+        acc_meter.reset_states()
